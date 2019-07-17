6ce455352eb937cea46eaa7eb4687a1dcb7238d5
AmphetamineLogic <phoenix.public@gmail.com>
2019-07-17 21:47:50 +0300

    Stage 3 completed, files moved

--- Game of Life/task/src/life/Generator.java
+++ Game of Life/task/src/life/Generator.java
@@ -25,8 +25,9 @@ class Generator {
     }
 
     void evolute() {
-        for (int i = 0; i < numberOfGenerations; i++) {
+        for (int i = 1; i <= numberOfGenerations; i++) {
             createNextGen();
+            universe.printStateWithStats(i);
         }
     }
 
@@ -74,7 +75,6 @@ class Generator {
         }
 
         universe.setMap(nextGeneration);
-//        universe.printState();
     }
 
     private char invertCell(char currentState) {
